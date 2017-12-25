9dc9600cf26fc6268a3bcba5056758e8d28b710f
Lisa Seelye <18159+lisa@users.noreply.github.com>
2017-12-24 23:23:38 -0500

    Complete Day 1 part B (#4)
    Rework the part A solution to use
    [container/ring](https://golang.org/pkg/container/ring/) so that it's
    easier to complete part B.
    
    Part B looks at the digit at position len/2 instead of len+1.

--- 2017/day01.go
+++ 2017/day01.go
@@ -1,13 +1,19 @@
 package main
 
-/* Compute the sum of input digits with these rules:
+/* Part A: Compute the sum of input digits with these rules:
 
 1. The input is circular (wraps around) so i[len(i)+1] == i[0]
 2. Do work (add) if i[n] == i[n+1] where n is the candidate digit.
 
+Part B:
+
+1. The input is circular (wraps around) so i[len(i)+1] == i[0]
+2. Do work (add) iff i[n] == i[len(i)/2]
+
 */
 
 import (
+	"container/ring"
 	"flag"
 	"fmt"
 	"strconv"
@@ -15,32 +21,38 @@ import (
 )
 
 var input = flag.String("input", "1122", "Digits to use")
+var partB = flag.Bool("partB", false, "Perform part B solution?")
 
 func main() {
 	flag.Parse()
 
-	digits := make([]int, len(*input))
-	for i, d := range strings.Split(*input, "") {
+	digits := ring.New(len(*input))
+	for _, d := range strings.Split(*input, "") {
 		digit, err := strconv.Atoi(d)
 		if err != nil {
 			fmt.Printf("Couldn't convert %d to a string: %v", d, err)
 			return
 		}
-		digits[i] = digit
+		digits.Value = digit
+		digits = digits.Next()
 	}
+	// Zip back to the "start"
+	digits = digits.Move(-1 * digits.Len())
 	sum := 0
 
-	for n := 0; n < len(digits); n++ {
-		var lookup int
-		if n == len(digits)-1 {
-			// need to look at 0
-			lookup = 0
+	for n := 0; n < digits.Len(); n++ {
+		if *partB {
+			// Part B logic, compare to position at len/2
+			if digits.Value == digits.Move(digits.Len()/2).Value.(int) {
+				sum += digits.Value.(int)
+			}
 		} else {
-			lookup = n + 1
+			// Part A logic
+			if digits.Value == digits.Next().Value.(int) {
+				sum += digits.Value.(int)
+			}
 		}
-		if digits[n] == digits[lookup] {
-			sum += digits[n]
-		}
+		digits = digits.Next()
 	}
 
 	fmt.Printf("Sum: %d\n", sum)
