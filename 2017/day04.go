abe545a5815ed175ed69cd17317d61889f122e62
Lisa Seelye <lisa@users.noreply.github.com>
2017-12-26 12:24:50 -0500

    Day 4 Part B solution

--- 2017/day04.go
+++ 2017/day04.go
@@ -14,7 +14,6 @@ Part B:
 
 Passphrases aren't valid if it contains two words which are anagrams of each other.
 NOTE: Part A is a specific case of being an anagram.
-
 */
 
 import (
@@ -25,7 +24,37 @@ import (
 	"strings"
 )
 
-var inputFile = flag.String("inputFile", "inputs/day04-example.txt", "Input file for Day 4")
+var inputFile = flag.String("inputFile", "inputs/day04b-example.txt", "Input file for Day 4")
+var partB = flag.Bool("partB", false, "Perform part B solution?")
+
+// Is needle an anagram of haystack?
+func isAnagram(needle, haystack string) bool {
+	// if they're a different length it is obviously not an angram
+	if len(needle) != len(haystack) {
+		return false
+	}
+
+	needle_freq := make(map[string]int)
+	haystack_freq := make(map[string]int)
+
+	// at this point the needle and haystack are the same length, so this loop is safe
+	for i := 0; i < len(needle); i++ {
+		needle_freq[needle[i:i+1]] += 1
+		haystack_freq[haystack[i:i+1]] += 1
+	}
+
+	// go through needle_freq and compare to haystack_freq
+	for str, freq := range needle_freq {
+		hay_str_freq, ok := haystack_freq[str]
+		// if it's not in the haystack, can't be an anagram
+		// if the freq of `str` in both aren't identical, not an anagram
+		if !ok || hay_str_freq != freq {
+			return false
+		}
+	}
+
+	return true
+}
 
 func main() {
 	flag.Parse()
@@ -42,19 +71,36 @@ func main() {
 			words = append(words, word)
 		}
 		// We have the candidate passphrase in `words`, let's analyze it.
+
 		var valid bool
 		valid = true
-		for i := 0; i < len(words); i++ {
-			for j := i + 1; j < len(words); j++ {
-				if words[i] == words[j] {
-					valid = false
-					break
+		if *partB {
+			// take the words and compare against each other if it's anagram
+		outer:
+			for i := 0; i < len(words); i++ {
+				for j := i + 1; j < len(words); j++ {
+					valid = !isAnagram(words[i], words[j])
+					if !valid {
+						break outer
+					}
 				}
 			}
-		}
+		} else {
+
+			for i := 0; i < len(words); i++ {
+				for j := i + 1; j < len(words); j++ {
+					if words[i] == words[j] {
+						valid = false
+						break
+					}
+				}
+			}
+		} // end part b check
+
 		if valid {
 			validPassphrases += 1
 		}
+
 	}
 	fmt.Printf("Valid passphrases: %d\n", validPassphrases)
 }
