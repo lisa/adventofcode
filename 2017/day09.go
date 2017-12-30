34bdf00f80b49a4d8fe5516f806e2997074e66df
Lisa Seelye <lisa@users.noreply.github.com>
2017-12-30 14:43:28 -0500

    Day 9 part B solution

--- 2017/day09.go
+++ 2017/day09.go
@@ -23,8 +23,9 @@ Scoring:
 Each group is assigned a score which is one more than the score of the group
 that immediately contains it. (The outermost group gets a score of 1.)
 
-
-
+Part B:
+Count the number of characters in garbage. <> don't count towards the count and
+neither does ! nor the character(s) being cancelled by !.
 */
 
 import (
@@ -34,14 +35,16 @@ import (
 )
 
 var input = flag.String("input", "{}", "Input string for the program")
+var partB = flag.Bool("partB", false, "Perform part B solution")
 var debug = flag.Bool("debug", false, "Debug output")
 
 func main() {
 	flag.Parse()
 	// Loop over each character
 	characters := strings.Split(*input, "")
-	depth := 0 // Group depth; used with scoring
-	score := 0 // Total score
+	depth := 0             // Group depth; used with scoring
+	score := 0             // Total score
+	garbageCharacters := 0 // Cleaned up garbage characters
 	ignoreNext := false
 	ignoreGroups := false
 	for i, token := range characters {
@@ -57,8 +60,14 @@ func main() {
 		case "{":
 			if !ignoreGroups {
 				depth += 1
+			} else {
+				garbageCharacters += 1
 			}
 		case "<":
+			if ignoreGroups {
+				// Already ignoring, so we need to bump garbageCharacters
+				garbageCharacters += 1
+			}
 			ignoreGroups = true // start of garbage
 		case ">":
 			ignoreGroups = false // end of garbage
@@ -66,10 +75,21 @@ func main() {
 			if !ignoreGroups {
 				score += depth
 				depth -= 1
+			} else {
+				garbageCharacters += 1
 			}
 		case "!":
 			ignoreNext = true
+		default:
+			if ignoreGroups {
+				garbageCharacters += 1
+			}
 		}
 	} // EOF
-	fmt.Printf("high score: %d\n", score)
+
+	if *partB {
+		fmt.Printf("Garbage characters: %d\n", garbageCharacters)
+	} else {
+		fmt.Printf("high score: %d\n", score)
+	}
 }
