ecb0679b0d3a143edda5eb03c7f716671be421d8
Lisa Seelye <18159+lisa@users.noreply.github.com>
2018-12-18 18:54:37 -0500

    Day 9 part B (#62)
    Previous solution took just under 5 hours to compute the wrong answer
    (trivially wrong) due to a misreading in the puzzle. My misreading lead
    me to track the winning elf from part A on into part B, which is wrong.
    
    This commit adds a solution which computes part A in 0.021 seconds on a
    mid 2015 MacBook Air and completes part B in just over a second, at
    1.079 seconds, on the same hardware.
    
    "Laughing at the speed of the answer, indeed."

--- 2018/day09.go
+++ 2018/day09.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
+	"math"
 	"os"
 	"strconv"
 	"strings"
@@ -24,143 +25,125 @@ func errorIf(msg string, e error) {
 	}
 }
 
-// based on https://stackoverflow.com/questions/43018206/modulo-of-negative-integers-in-go
-// why oh why does Go behave differently :'(
-// ruby: -1 % 4 == 3
-// go:   -1 % 4 == -1
-func rubyMod(d, m int) int {
-	r := d % m
-	if (r < 0 && m > 0) || (r > 0 && m < 0) {
-		r += m
-	}
+// InitCircle - Start the loop
+func InitCircle() *Marble {
+	r := NewMarble(0)
+	r.Left = r
+	r.Right = r
 	return r
 }
 
-// Len - how many marbles in the circle?
-func (c *Circle) Len() int {
-	return int(len(c.Marbles))
-}
-
-// Circle - where all the marbles go
-type Circle struct {
-	CurrentMarbleIndex int
-	Marbles            []int
-}
-
-func NewCircle() *Circle {
-	c := Circle{
-		CurrentMarbleIndex: 0,
-		Marbles:            []int{0},
+// NewMarble - make a new Marble
+func NewMarble(v int) *Marble {
+	return &Marble{
+		Value: v,
+		Left:  nil,
+		Right: nil,
 	}
-	c.Marbles[0] = 0
-	return &c
 }
 
-// InsertMarble - inserts a marble according to the rules
-func (c *Circle) InsertMarble(value int) {
-	// Insert the marble between the marbles 1 and 2 clockwise (left) of the current index
-	// [a] [b] [c] (d) [e] [f] [g] d = current
-	// Insert between [e] and [f]
-	oneEdge := rubyMod(c.CurrentMarbleIndex+1, c.Len())
-	twoEdge := rubyMod(c.CurrentMarbleIndex+2, c.Len())
-	var nextIndex int
-	var leftPart, rightPart []int
-	switch len(c.Marbles) {
-	case 1:
-		c.Marbles = []int{0, 1}
-		c.CurrentMarbleIndex = 1
-		return
-	case 2:
-		leftPart = []int{c.Marbles[0]}
-		rightPart = []int{c.Marbles[1]}
-		nextIndex = 1
-	case 3:
-		leftPart = []int{c.Marbles[0], c.Marbles[1], c.Marbles[2]}
-		rightPart = []int{}
-		nextIndex = 3
-	default:
-		if twoEdge < oneEdge {
-			leftPart = c.Marbles[0:twoEdge]
-			rightPart = c.Marbles[twoEdge : oneEdge+1]
-			nextIndex = twoEdge
-		} else {
-			leftPart = c.Marbles[0 : oneEdge+1]
-			rightPart = c.Marbles[oneEdge+1:]
-			nextIndex = rubyMod(oneEdge+1, c.Len()+1)
-		}
-	}
-	// insert the new marble
-	c.Marbles = append(leftPart, append([]int{value}, rightPart...)...)
-	c.CurrentMarbleIndex = nextIndex
-	return
+// Marble - the Marble object; a doubly linked list.
+type Marble struct {
+	Right *Marble // clockwise
+	Left  *Marble // counterclockwise
+	Value int     // value of this marble
 }
 
-// RemoveAtIndex - removes the marble at index i and returns its value.
-// The marble to the right (clockwise) of the removed marble becomes the current index
-func (c *Circle) RemoveAtIndex(i int) int {
-	v := c.Marbles[i]
-	newIndex := c.FindIndexByOffset(-6)
-	if newIndex == 0 {
-		// we're removing the last element so let's help out a little since the math
-		// below doesn't work
-		c.Marbles = c.Marbles[0 : c.Len()-1]
-		c.CurrentMarbleIndex = 0
-		return v
-	}
-	c.CurrentMarbleIndex = newIndex
-	// now cut it out
-	leftSide := c.Marbles[:rubyMod(c.CurrentMarbleIndex-1, c.Len())]
-	rightSide := c.Marbles[c.CurrentMarbleIndex:]
-	c.Marbles = append(leftSide, rightSide...)
-	c.MoveLeft()
-	return v
-}
-
-// FindIndexByOffset - find the index if we would move +offset+.
-func (c *Circle) FindIndexByOffset(offset int) int {
+// Move - Traverse +offset+ Marbles.
+// If +offset+ is positive, move that many to the right (clockwise)
+// If +offset+ is negative, move that many to the left (counter-clockwise)
+func (m *Marble) Move(offset int) *Marble {
+	ret := m
 	if offset == 0 {
-		return c.CurrentMarbleIndex
+		return ret
 	}
-	return rubyMod(c.CurrentMarbleIndex+offset, int(len(c.Marbles)))
-}
-
-// Move offset % len(circle), move right +, move left -
-// "Move" means to move the CurrentMarbleIndex by that many.
-func (c *Circle) Move(offset int) {
-	c.CurrentMarbleIndex = c.FindIndexByOffset(offset)
-}
-
-// MoveLeft - move the index one to the left (counter-clockwise)
-// oddly we don't ever need to go Right.
-func (c *Circle) MoveLeft() {
-	c.Move(-1)
-}
-
-// Mod23 - Do the work necessary to handle when the added value is a multiple of
-// 23. Remove the marble 7 counter-clockwise (left) of the current marble
-// (return it) The marble to the right (clockwise) of the removed marble is the
-// new current.
-func (c *Circle) Mod23() int {
-	// [a] [b] [c] [d] [e] [f] [g] [h] [i] [j] (k) [l] [m] [n]
-	// Remove [d]
-	// New index is [e]
-	v := c.RemoveAtIndex(c.FindIndexByOffset(-7))
-	return v
-}
-
-func (c *Circle) String() string {
-	ret := ""
-	var i int
-	for i = 0; i < c.Len(); i++ {
-		if i == c.CurrentMarbleIndex {
-			ret += fmt.Sprintf(" (%d) ", c.Marbles[i])
+	for i := 0; i < int(math.Abs(float64(offset))); i++ {
+		if offset < 0 {
+			ret = ret.Left
+			//fmt.Printf("Moving left -> %+v\n", ret)
 		} else {
-			ret += fmt.Sprintf(" %d ", c.Marbles[i])
+			ret = ret.Right
+			//fmt.Printf("Moving right -> %+v\n", ret)
 		}
 	}
 	return ret
 }
 
+// Delete - Delete the current Marble; its Value is returned. Left and Right associations are carried over
+func (m *Marble) Delete() int {
+	ret := m.Value
+	l := m.Left
+	r := m.Right
+	r.Left = l
+	l.Right = r
+	return ret
+}
+
+// InsertRight - insert a Marble with value +v+ to the Right (clockwise) of myself. Return the new Marble
+// new Marble is to my right
+func (m *Marble) InsertRight(v int) *Marble {
+	n := NewMarble(v)
+
+	// new marble and old "right of me"
+	m.Right.Left = n
+	n.Right = m.Right
+
+	n.Left = m
+	m.Right = n
+
+	return n
+}
+
+// InsertLeft - insert a Marble with value +v+ to the Left (counterclockwise) of myself. Return the new Marble.
+func (m *Marble) InsertLeft(v int) *Marble {
+	n := NewMarble(v)
+
+	m.Left.Right = n
+	n.Left = m.Left
+
+	m.Left = n
+	n.Right = m
+
+	return n
+}
+
+// PrintCircle - Print a representation of the Marble circle
+func PrintCircle(current *Marble) {
+	currentIntVal := current.Value
+	currentPtrVal := current
+
+	for {
+		current = current.Right
+		if current.Value == currentIntVal {
+			fmt.Printf("(%d)\n", current.Value)
+			break
+		} else {
+			fmt.Printf("%d ", current.Value)
+		}
+	}
+	current = currentPtrVal
+}
+
+// PartA - do part A and return the scores
+func PartA(players, lastMarbleValue int) map[int]int {
+	current := InitCircle()
+
+	// player -> score
+	score := make(map[int]int)
+
+	var del *Marble
+	for v := 1; v <= lastMarbleValue; v++ {
+		if v%23 == 0 {
+			del = current.Move(-7)
+			current = del.Right
+			score[v%players] += v + del.Delete()
+		} else {
+			current = current.Right.InsertRight(v)
+		}
+	}
+	return score
+
+}
 func main() {
 	flag.Parse()
 
@@ -180,28 +163,24 @@ func main() {
 
 	fmt.Printf("Player count %d, highest marble value %d\n", players, lastMarbleValue)
 
-	score := make([]int, int(players))
+	if !*partB {
+		score := PartA(players, lastMarbleValue)
 
-	circle := NewCircle()
-
-	var v int
-	for v = 1; v <= int(lastMarbleValue); v++ {
-		if v%23 == 0 {
-
-			seventh := circle.Mod23()
-
-			removed[v] = seventh
-			score[v%int(players)] += seventh + v
-		} else {
-			circle.InsertMarble(v)
+		highScore := -1
+		for i := range score {
+			if score[i] > highScore {
+				highScore = score[i]
+			}
 		}
-	}
-
-	highest := -1
-	for i := range score {
-		if score[i] > highest {
-			highest = score[i]
+		fmt.Printf("Part A high score %d\n", highScore)
+	} else {
+		highScore := -1
+		score := PartA(players, lastMarbleValue*100)
+		for i := range score {
+			if score[i] > highScore {
+				highScore = score[i]
+			}
 		}
+		fmt.Printf("Part B high score %d\n", highScore)
 	}
-	fmt.Printf("high score = %d\n", highest)
 }
