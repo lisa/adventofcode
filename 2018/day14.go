5a54b5f6aa40f0e4ddcd5aa980604ab636e01086
Lisa Seelye <lisa@users.noreply.github.com>
2018-12-31 14:55:30 -0500

    Day 14 part B
    Moved to uint8 along the way.

--- 2018/day14.go
+++ 2018/day14.go
@@ -4,10 +4,12 @@ import (
 	"flag"
 	"fmt"
 	"math"
+	"os"
+	"strconv"
 )
 
 var (
-	input = flag.Int("input", 681901, "Input Data")
+	input = flag.String("input", "681901", "Input Data")
 	partB = flag.Bool("partB", false, "do part b?")
 	debug = flag.Bool("debug", false, "debug")
 )
@@ -20,24 +22,52 @@ func rubyMod(d, m int) int {
 	return r
 }
 
+// ScanForSequence - for the needle in the haystack. If it exists in the
+// haystack, return true.
+func ScanForSequence(needle, haystack *[]uint8, offset int) bool {
+	if len(*haystack) < len(*needle) {
+		return false
+	}
+
+	ret := true
+	j := 0
+	for i := len(*haystack) - len(*needle) - offset; i < len(*haystack) && j < len(*needle); i++ {
+		if i < 0 {
+			continue
+		}
+		ret = ret && (*haystack)[i] == (*needle)[j]
+		j++
+		if !ret {
+			break
+		}
+	}
+	return ret
+
+}
+
+// CountDigits - how many digits are in the given integer?
+func CountDigits(number int) int {
+	if number == 0 {
+		return 1
+	}
+	return int(math.Floor(math.Log10(math.Abs(float64(number)))) + 1)
+}
+
 // SplitDigits - Splits +number+ into its digit components.
 // The return will be in the same order
 // 681901 will be returned [6, 8, 1, 9, 0, 1]
-func SplitDigits(number int) []int {
+func SplitDigits(number int) []uint8 {
 	// we have nothing, so add nothing
 	if number == 0 {
-		return []int{0}
+		return []uint8{0}
 	}
-	digitCount := int(math.Floor(math.Log10(float64(number))) + 1)
-	if *debug {
-		fmt.Printf("Digit count vs number: %d : %d\n", digitCount, number)
-	}
-	ret := make([]int, digitCount)
+	digitCount := CountDigits(number)
+	ret := make([]uint8, digitCount)
 
 	i := number
 	for d := 0; d < digitCount; d++ {
 		digit := i % 10
-		ret[digitCount-d-1] = digit
+		ret[digitCount-d-1] = uint8(digit)
 		i -= digit
 		i /= 10
 	}
@@ -46,53 +76,74 @@ func SplitDigits(number int) []int {
 
 // function to split the sum into digits (for iteration/addition)
 // need Ruby Mod function again
+func errorIf(msg string, e error) {
+	if e != nil {
+		fmt.Printf("%s\n")
+		os.Exit(1)
+	}
+}
 
 func main() {
 	flag.Parse()
 
-	scores := []int{3, 7}
+	scores := []uint8{3, 7}
 
 	elf1Index := 0
 	elf2Index := 1
 
-	// less than, since we've already added 1 ([3,7])
-	for len(scores) < *input+10 {
-		if *debug {
-			fmt.Printf("len=%d elf 1 index=%d value=%d, elf2 index=%d value=%d\n",
-				len(scores), elf1Index, scores[elf1Index], elf2Index, scores[elf2Index])
+	if !*partB {
+		inputNumber, err := strconv.Atoi(*input)
+		errorIf("couldnt parse input", err)
+		// less than, since we've already added 1 ([3,7])
+		for len(scores) < inputNumber+10 {
+			digits := SplitDigits(int(scores[elf1Index] + scores[elf2Index]))
+			// append to scores
+			scores = append(scores, digits...)
+			// move forward
+			newElf1Idx := 1 + int(scores[elf1Index]) + elf1Index
+			newElf2Idx := 1 + int(scores[elf2Index]) + elf2Index
+			elf1Index = rubyMod(newElf1Idx, len(scores))
+			elf2Index = rubyMod(newElf2Idx, len(scores))
 		}
-		digits := SplitDigits(scores[elf1Index] + scores[elf2Index])
-		// append to scores
-		scores = append(scores, digits...)
-		// move forward
-		newElf1Idx := 1 + scores[elf1Index] + elf1Index
-		newElf2Idx := 1 + scores[elf2Index] + elf2Index
-		elf1Index = rubyMod(newElf1Idx, len(scores))
-		elf2Index = rubyMod(newElf2Idx, len(scores))
-		if *debug {
-			fmt.Printf("scores: %d\n", scores)
+		ten := scores[inputNumber : inputNumber+10]
+		sum := 0
+		sum += int(ten[0]) * 1000000000
+		sum += int(ten[1]) * 100000000
+		sum += int(ten[2]) * 10000000
+		sum += int(ten[3]) * 1000000
+		sum += int(ten[4]) * 100000
+		sum += int(ten[5]) * 10000
+		sum += int(ten[6]) * 1000
+		sum += int(ten[7]) * 100
+		sum += int(ten[8]) * 10
+		sum += int(ten[9]) * 1
+
+		fmt.Printf("Score of the ten recipes: %010d\n", sum)
+	} else {
+		needle := make([]uint8, len(*input))
+		for i, r := range *input {
+			n, err := strconv.ParseInt(string(r), 10, 0)
+			errorIf("couldnt parse input", err)
+			needle[i] = uint8(n)
+		}
+		for {
+
+			digits := SplitDigits(int(scores[elf1Index] + scores[elf2Index]))
+			// append to scores
+			scores = append(scores, digits...)
+			// move forward
+			newElf1Idx := 1 + int(scores[elf1Index]) + elf1Index
+			newElf2Idx := 1 + int(scores[elf2Index]) + elf2Index
+			elf1Index = rubyMod(newElf1Idx, len(scores))
+			elf2Index = rubyMod(newElf2Idx, len(scores))
+			// only need to look at the last 5 digits of the score.
+			if len(scores) >= len(needle) {
+				if ScanForSequence(&needle, &scores, len(digits)-1) {
+					fmt.Printf("%s occurs after %d\n",
+						*input, len(scores)-len(*input)-len(digits)+1)
+					break
+				}
+			}
 		}
 	}
-	if *debug {
-		fmt.Printf("All done - Elf 1 index: %d Elf 2 index: %d\n", elf1Index, elf2Index)
-
-		fmt.Printf("Scores: %d\n", scores)
-
-		fmt.Printf("Slice off ten: %d\n", scores[*input:*input+10])
-	}
-	ten := scores[*input : *input+10]
-
-	sum := ten[0] * 1000000000
-	sum += ten[1] * 100000000
-	sum += ten[2] * 10000000
-	sum += ten[3] * 1000000
-	sum += ten[4] * 100000
-	sum += ten[5] * 10000
-	sum += ten[6] * 1000
-	sum += ten[7] * 100
-	sum += ten[8] * 10
-	sum += ten[9] * 1
-
-	fmt.Printf("Score of the ten recipes: %010d\n", sum)
-
 }
