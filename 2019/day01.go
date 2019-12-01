90c8d7730e1315fe30cd0c8c11f1bb0108a41c18
Lisa Seelye <18159+lisa@users.noreply.github.com>
2019-12-01 12:04:11 -0500

    2019 day 1 part B (#76)
    Signed-off-by: Lisa Seelye <lisa@users.noreply.github.com>

--- 2019/day01.go
+++ 2019/day01.go
@@ -16,36 +16,54 @@ var (
 	debug       = flag.Bool("debug", false, "Debug?")
 )
 
-func Compute(m int) int {
+func compute(m int) int {
 	return (int(math.Floor(float64(m)/3)) - 2)
+}
+
+func Compute(m int) int {
+	ret := 0
+	if !*partB {
+		return compute(m)
+	} else {
+		if *debug {
+			fmt.Printf("Computing part B\n")
+		}
+		newmass := compute(m)
+		fmt.Printf("Mass %d turns into %d\n", m, newmass)
+		for {
+			if newmass > 0 {
+				ret += newmass
+				fmt.Printf("Computing mass for fuel needed for %d\n", newmass)
+				newmass = compute(newmass)
+			} else {
+				fmt.Printf("Wishing really hard for mass %d\n", newmass)
+				return ret
+			}
+		}
+	}
 }
 
 func main() {
 	flag.Parse()
 
-	if !*partB {
-		// Part A
-		input, err := os.Open(*inputFile)
+	input, err := os.Open(*inputFile)
+	if err != nil {
+		fmt.Printf("Couldn't open %s: %v\n", *inputFile, err)
+		os.Exit(1)
+	}
+
+	sum := 0
+	lineReader := bufio.NewScanner(input)
+	for lineReader.Scan() {
+		line := lineReader.Text()
+		number, err := strconv.Atoi(line)
 		if err != nil {
-			fmt.Printf("Couldn't open %s: %v\n", *inputFile, err)
+			fmt.Printf("Couldn't parse %s: %e\n", line, err)
 			os.Exit(1)
 		}
-	
-		sum := 0
-		lineReader := bufio.NewScanner(input)
-		for lineReader.Scan() {
-			line := lineReader.Text()
-			number, err := strconv.Atoi(line)
-			if err != nil {
-				fmt.Printf("Couldn't parse %s: %e\n", line, err)
-				os.Exit(1)
-			}
-	
-			sum += Compute(number)
-		}
-		fmt.Printf("Sum: %d\n",sum)
 
-	} else {
-		fmt.Printf("Part B\n")
+		sum += Compute(number)
 	}
+	fmt.Printf("Sum: %d\n", sum)
+
 }
