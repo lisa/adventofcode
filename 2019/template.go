08f642ad158ab6750040fe62c6a2cea1573ca7cf
Lisa Seelye <18159+lisa@users.noreply.github.com>
2019-12-01 12:23:55 -0500

    Improve template
    Now, when I save it in vscode it doesn't remove the imports.
    
    (although I'm surprised it doesn't complain about `os.exit(0)` as the last thing in `main`.)

--- 2019/template.go
+++ 2019/template.go
@@ -7,12 +7,21 @@ import (
 )
 
 var (
-	partB = flag.Bool("partB", false, "Perform part B solution?")
-	inputFile = flat.String("inputFile", "inputs/day!DAY!a.txt", "Input File")
+	partB       = flag.Bool("partB", false, "Perform part B solution?")
+	inputFile   = flag.String("inputFile", "inputs/day!DAY!a.txt", "Input File")
 	inputString = flag.String("input", "", "Input string")
-	debug = flag.Bool("debug", false, "Debug?")
+	debug       = flag.Bool("debug", false, "Debug?")
 )
 
 func main() {
 	flag.Parse()
+
+	if !*partB {
+		// part A
+		fmt.Printf("Part A")
+	} else {
+		// part B
+	}
+
+	os.Exit(0)
 }
