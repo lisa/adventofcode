da25f537621f268918efa9f7546465aa7e2ee5b5
Lisa Seelye <18159+lisa@users.noreply.github.com>
2019-12-02 17:34:03 -0500

    2019 Day 2 part B (#80)
    Signed-off-by: Lisa Seelye <lisa@users.noreply.github.com>

--- 2019/day02.go
+++ 2019/day02.go
@@ -9,7 +9,8 @@ import (
 )
 
 const (
-	inputText string = `1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,9,1,19,1,19,5,23,1,9,23,27,2,27,6,31,1,5,31,35,2,9,35,39,2,6,39,43,2,43,13,47,2,13,47,51,1,10,51,55,1,9,55,59,1,6,59,63,2,63,9,67,1,67,6,71,1,71,13,75,1,6,75,79,1,9,79,83,2,9,83,87,1,87,6,91,1,91,13,95,2,6,95,99,1,10,99,103,2,103,9,107,1,6,107,111,1,10,111,115,2,6,115,119,1,5,119,123,1,123,13,127,1,127,5,131,1,6,131,135,2,135,13,139,1,139,2,143,1,143,10,0,99,2,0,14,0`
+	inputText  string = `1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,9,1,19,1,19,5,23,1,9,23,27,2,27,6,31,1,5,31,35,2,9,35,39,2,6,39,43,2,43,13,47,2,13,47,51,1,10,51,55,1,9,55,59,1,6,59,63,2,63,9,67,1,67,6,71,1,71,13,75,1,6,75,79,1,9,79,83,2,9,83,87,1,87,6,91,1,91,13,95,2,6,95,99,1,10,99,103,2,103,9,107,1,6,107,111,1,10,111,115,2,6,115,119,1,5,119,123,1,123,13,127,1,127,5,131,1,6,131,135,2,135,13,139,1,139,2,143,1,143,10,0,99,2,0,14,0`
+	partBValue int    = 19690720
 )
 
 var (
@@ -18,6 +19,53 @@ var (
 	inputString = flag.String("input", inputText, "Input string")
 	debug       = flag.Bool("debug", false, "Debug?")
 )
+
+func copySlice(i []int) []int {
+	o := make([]int, len(i))
+	for i, v := range i {
+		o[i] = v
+	}
+	return o
+}
+
+// runProgram - runs a program, returns the finished product as output
+func runProgram(program []int) int {
+	if *debug {
+		fmt.Printf("Running program: %d\n", program)
+	}
+	cursor := 0
+	for {
+		var opcode, left, right, dest int
+		opcode = program[cursor]
+		if opcode == 99 {
+			if *debug {
+				fmt.Printf("Found opcode 99, returning...\n")
+			}
+			return program[0]
+		}
+		left = program[cursor+1]
+		right = program[cursor+2]
+		dest = program[cursor+3]
+
+		var result int
+		switch opcode {
+		case 1:
+			result = program[left] + program[right]
+			if *debug {
+				fmt.Printf("[cursor: %d; [%d, %d, %d, %d]; n: %d]: %d + %d = %d.\n",
+					cursor, opcode, left, right, dest, cursor+4, program[left], program[right], result)
+			}
+		case 2:
+			result = program[left] * program[right]
+			if *debug {
+				fmt.Printf("[cursor: %d; [%d, %d, %d, %d]; n: %d]: %d * %d = %d.\n",
+					cursor, opcode, left, right, dest, cursor+4, program[left], program[right], result)
+			}
+		}
+		program[dest] = result
+		cursor += 4
+	}
+}
 
 func main() {
 	flag.Parse()
@@ -33,51 +81,34 @@ func main() {
 		program = append(program, n)
 	}
 
-	// Replace
-	program[1] = 12
-	program[2] = 2
-	if *debug {
-		fmt.Printf("Full program: %+v\n", program)
-	}
 	if !*partB {
 		// part A
 		// go through program
-		cursor := 0
-		for {
-			var opcode, left, right, dest int
-			opcode = program[cursor]
-			if opcode != 99 {
-				left = program[cursor+1]
-				right = program[cursor+2]
-				dest = program[cursor+3]
-			}
-			var result int
-			switch opcode {
-			case 1:
 
-				result = program[left] + program[right]
-				if *debug {
-					fmt.Printf("[cursor: %d; [%d, %d, %d, %d]; n: %d]: %d + %d = %d.\n",
-						cursor, opcode, left, right, dest, cursor+4, program[left], program[right], result)
-				}
-			case 2:
-				result = program[left] * program[right]
-				if *debug {
-					fmt.Printf("[cursor: %d; [%d, %d, %d, %d]; n: %d]: %d: %d * %d = %d.\n",
-						cursor, opcode, left, right, dest, cursor+4, program[left], program[right], result)
-				}
-			case 99:
-				fmt.Printf("End of program. Position 0 value: %d\n", program[0])
-				if *debug {
-					fmt.Printf("Program at end: %+v\n", program)
-				}
-				os.Exit(0)
-			}
-			program[dest] = result
-			cursor += 4
-		}
+		// Set for part A
+		program[1] = 12
+		program[2] = 2
+		result := runProgram(program)
+		fmt.Printf("Part A value at position 0: %d\n", result)
+
 	} else {
 		// part B
+		for verb := 0; verb < 100; verb++ {
+			for noun := 0; noun < 100; noun++ {
+
+				programCopy := copySlice(program)
+				programCopy[1] = noun
+				programCopy[2] = verb
+				if *debug {
+					fmt.Printf("Verb %d, Noun %d. First 3 bytes: %d\n", verb, noun, programCopy[:3])
+				}
+
+				if result := runProgram(programCopy); result == partBValue {
+					fmt.Printf("Found %d: Verb %d, noun %d. 100 * noun + verb = %d\n", partBValue, verb, noun, 100*noun+verb)
+					os.Exit(0)
+				}
+			}
+		}
 	}
 
 	os.Exit(0)
